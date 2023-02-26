d3bc07783906063db60bb82cc87bb4b536965478
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-26 16:01:22 +0700

    LineByLineArrayTask solved

--- LineByLineArrayTask/Program.cs
+++ LineByLineArrayTask/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿//Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+void FillArray(int[,,] array)
+{
+    int element = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = element;
+                element++;
+            }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] array = new int[3, 3, 3];
+FillArray(array);
+PrintArray(array);
