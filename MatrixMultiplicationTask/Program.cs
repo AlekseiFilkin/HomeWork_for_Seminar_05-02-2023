9b233ef148797f1f9496408c14d8fab28e3f6d18
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-26 15:01:03 +0700

    MatrixMultiplicationTask solved

--- MatrixMultiplicationTask/Program.cs
+++ MatrixMultiplicationTask/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿//Найти произведение двух матриц
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] Multiplication(int[,] array1, int[,] array2)
+{
+    int[,] array3 = new int[array1.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                array3[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return array3;
+}
+Console.Write("Введите количество строк двумерного массива: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов двумерного массива: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+int[,] array1 = new int[m, n];
+int[,] array2 = new int[m, n];
+FillArray(array1);
+PrintArray(array1);
+FillArray(array2);
+PrintArray(array2);
+PrintArray(Multiplication(array1, array2));
